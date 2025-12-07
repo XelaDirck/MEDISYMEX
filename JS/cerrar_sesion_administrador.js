c17040999053d1542265d96b16b61e14ea271895
XelaDirck <129549557+XelaDirck@users.noreply.github.com>
2025-12-06 21:45:20 -0600

    Add files via upload

---
+++ JS/cerrar_sesion_administrador.js
@@ -0,0 +1,7 @@
+function cerrarSesion() {
+    // Confirmar antes de cerrar sesión
+    if (confirm('¿Estás seguro de que deseas cerrar sesión?')) {
+        // Redirigir al archivo de logout
+        window.location.href = '../PHP/logout_administrador.php';
+    }
+}
