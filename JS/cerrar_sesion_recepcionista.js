59023fbdb7c2ff4440a808df28a5d7b2164e0bf5
XCyberDjent <XCyberDjent@outlook.com>
2025-12-08 02:24:15 -0600

    Delete JS directory

--- JS/cerrar_sesion_recepcionista.js
+++
@@ -1,7 +0,0 @@
-function cerrarSesion() {
-    // Confirmar antes de cerrar sesión
-    if (confirm('¿Estás seguro de que deseas cerrar sesión?')) {
-        // Redirigir al archivo de logout
-        window.location.href = '../PHP/logout_recepcionista.php';
-    }
-}
